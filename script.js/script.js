0df3a3e73640439bc2ee5697ae5887da40bf0a48
LucasSi7va <132783560+LucasSi7va@users.noreply.github.com>
2024-03-13 09:30:11 -0300

    atualizado

--- script.js/script.js
+++ script.js/script.js
@@ -13,3 +13,44 @@ document.addEventListener("DOMContentLoaded", function() {
       }
     });
   });
+
+
+
+  document.getElementById('cartao').addEventListener('input', function (e) {
+    let inputValue = e.target.value;
+    // Remove qualquer caractere que não seja número
+    inputValue = inputValue.replace(/\D/g, '');
+    // Insere os hifens nos lugares corretos
+    if (inputValue.length > 3) {
+        inputValue = inputValue.slice(0, 3) + '-' + inputValue.slice(3);
+    }
+    if (inputValue.length > 7) {
+        inputValue = inputValue.slice(0, 7) + '-' + inputValue.slice(7);
+    }
+    // Atualiza o valor do campo de entrada
+    e.target.value = inputValue;
+});
+
+
+
+document.getElementById('cartao2').addEventListener('input', function (e) {
+  let inputValue = e.target.value;
+  // Remove qualquer caractere que não seja número
+  inputValue = inputValue.replace(/\D/g, '');
+  // Insere os hifens nos lugares corretos
+  if (inputValue.length > 3) {
+      inputValue = inputValue.slice(0, 3) + '-' + inputValue.slice(3);
+  }
+  if (inputValue.length > 7) {
+      inputValue = inputValue.slice(0, 7) + '-' + inputValue.slice(7);
+  }
+  // Atualiza o valor do campo de entrada
+  e.target.value = inputValue;
+});
+
+
+var botao = document.getElementById("btn");
+
+botao.addEventListener("click", function() {
+  alert("foi hackado");
+}) 
